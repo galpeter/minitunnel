dcd81f5a1b69d5c4bbbbe16c52b608124c4759cf
Peter Gal <pgal.u-szeged@partner.samsung.com>
2018-11-06 19:47:03 +0100

    Add documentation for ReverseHTTPServer and clean up the code a bit

--- example/example_ondevice.js
+++ example/example_ondevice.js
@@ -1,6 +1,20 @@
+/* Copyright 2018-present Samsung Electronics Co., Ltd. and other contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var fs = require('fs');
 var ReverseHTTPServer = require('reversehttp').ReverseHTTPServer;
-
 
 var options = {
     shouldOpenNew: true,
@@ -27,7 +41,7 @@ var testServer = new ReverseHTTPServer(options, function(req, resp) {
         resp.write(contents);
         resp.write('');
     }
-//    resp.end();
+    resp.end();
 });
 
 testServer.on('connection', function(socket) {
@@ -47,7 +61,7 @@ testServer.on('data', function(socket, data) {
     console.log(data.toString());
 });
 
-testServer.requestTunnel('http://localhost:8889', function(tunnel_info) {
+testServer.requestTunnel('https://localhost:8889', function(tunnel_info) {
    console.log('Registered tunnel: ' + JSON.stringify(tunnel_info));
    testServer.listen(tunnel_info.remote_port, tunnel_info.remote_host);
 
